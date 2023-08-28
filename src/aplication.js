5cb9192916404b3b22f25bc5ad9b8db948b7c52c
EduardoS-Santos <116198821+EduardoS-Santos@users.noreply.github.com>
2023-08-27 23:43:40 -0300

    Update aplication.js

--- src/aplication.js
+++ src/aplication.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   console.log(`Date ${new Date()}`);
   next();
 });
+fetch("http://some-site.com/cors-disabled/some.json", { mode: "no-cors" });
 // porta de conexao
 const port = 8080;
 
