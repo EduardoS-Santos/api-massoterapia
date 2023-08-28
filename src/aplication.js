d1a36b6b9b91f35e587e432179cdeaec763c55ba
EduardoS-Santos <116198821+EduardoS-Santos@users.noreply.github.com>
2023-08-27 23:29:53 -0300

    Update aplication.js

--- src/aplication.js
+++ src/aplication.js
@@ -17,7 +17,9 @@ app.set("views", "src/views");
 //exemplo de middlewares
 app.use((req, res, next) => {
   header("Access-Control-Allow-Origin: *");
-
+  header(
+    "Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With"
+  );
   console.log(`Request Type: ${req.method}`);
   console.log(`Content Type: ${req.headers["content-type"]}`);
   console.log(`Date ${new Date()}`);
